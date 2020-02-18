524ab1f1ea9b62e1bd0906ac6a5326529735e08f
mfraggy25 <mfraggy25@users.noreply.github.com>
2020-02-18 18:42:51 -0500

    first draft task 3.8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { QuestionFormComponent } from './question-form/question-form.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+
+import { AppComponent } from './app.component';
+import { QuestionFormComponent } from './question-form/question-form.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ResultsComponent } from './results/results.component';
+import { QuestionsComponent } from './questions/questions.component';
+
+
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: ':quizId', component: QuestionsComponent },
+  { path: '', redirectTo: "welcome", pathMatch: "prefix" },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuestionFormComponent
+    QuestionFormComponent,
+    ResultsComponent,
+    WelcomeComponent,
+    QuestionsComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule, ReactiveFormsModule
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
